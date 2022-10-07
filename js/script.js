36e2597f566723d29e08ff1287d37a8fd5161c1b
hakerprof <hakerprof@gmail.com>
2022-10-07 20:10:11 +0300

    lesson_018

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-let numberOfFilms = prompt("How many movies have you seen already?", "0");
-console.log(numberOfFilms);
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("How many movies have you seen already?");
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("How many movies have you seen already?");
+    }
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,44 +17,43 @@ const personalMovieDB = {
     privat: false
 }
 
-// for (i = 1; i <= 2; i++) {
-//     let film = prompt(`Movie #${i} have you seen already?`, "");
-//     if (film != null && film !== "" && film.length <= 50) {
-//         let filmAssess = +prompt(`Your assessment of movie #${i}?`, "");
-//         personalMovieDB.movies[film] = filmAssess;
-//     } else {
-//         i = i - 1;
-//     }
-// }
-
-// let i = 1;
-// while (i <= 2) {
-//     let film = prompt(`Movie #${i} have you seen already?`, "");
-//     if (film != null && film !== "" && film.length <= 50) {
-//         let filmAssess = +prompt(`Your assessment of movie #${i}?`, "");
-//         personalMovieDB.movies[film] = filmAssess;
-//         i++;
-//     }
-// }
-
-let i = 1;
-do {
-    let film = prompt(`Movie #${i} have you seen already?`, "");
-    if (film != null && film !== "" && film.length <= 50) {
-        let filmAssess = +prompt(`Your assessment of movie #${i}?`, "");
-        personalMovieDB.movies[film] = filmAssess;
-        i++;
+function rememberMyFilms() {
+    for (let i = 1; i <= 2; i++) {
+        let film = prompt(`Movie #${i} have you seen already?`, "");
+        if (film != null && film !== "" && film.length <= 50) {
+            let filmAssess = +prompt(`Your assessment of movie #${i}?`, "");
+            personalMovieDB.movies[film] = filmAssess;
+        } else {
+            i = i - 1;
+        }
     }
 }
-while (i <= 5);
 
-console.log(personalMovieDB);
-if (personalMovieDB.count < 10 && personalMovieDB.count >= 0) {
-    alert("Переглянуто мало фільмів");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert("Ви класичний глядач");
-} else if (personalMovieDB.count >= 30) {
-    alert("Ви кіноман");
-} else {
-    alert("Сталася помилка");
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10 && personalMovieDB.count >= 0) {
+        alert("Переглянуто мало фільмів");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert("Ви класичний глядач");
+    } else if (personalMovieDB.count >= 30) {
+        alert("Ви кіноман");
+    } else {
+        alert("Сталася помилка");
+    }
 }
+
+rememberMyFilms();
+detectPersonalLevel();
+
+function showMyDB() {
+    if (personalMovieDB.privat == false){
+        console.log(personalMovieDB);
+    }
+}
+
+function writeYourGenres() {
+    for (let i = 1; i < 4; i ++){
+        personalMovieDB.genres[i-1] = prompt(`Ваш улюблений жанр під номером ${i}?`);
+    }
+}
+writeYourGenres();
+console.log(personalMovieDB);
